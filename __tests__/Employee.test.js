c5ba3c19001713df9bcdc0d940ff666c3e6b69bf
Kyle <kylejmbaylis@gmail.com>
2022-04-30 16:06:31 -0400

    uncommented id and email check, still passes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,6 +5,6 @@ test('creates an employee object', () => {
     const employee = new Employee('Kyle');
 
     expect(employee.name).toBe('Kyle');
-    // expect(employee.id).toEqual(expect.any(String));
-    // expect(employee.email).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(String));
+    expect(employee.email).toEqual(expect.any(String));
 });
